aa9f5ff2e2c4fec4a02d11ae1b299b250e7e4e8d
Mateusz Wisniewski <83494@student.ue.poznan.pl>
2024-11-03 13:58:47 +0100

    2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-nazwa_kategorii = input()
+nazwa_kategorii = input("Podaj nazwę kategorii: ")
 nazwa_kategorii = nazwa_kategorii.replace(" ", "_")
 url = "https://pl.wikipedia.org/wiki/Kategoria:" + nazwa_kategorii
 odpowiedz = requests.get(url)
@@ -16,9 +16,7 @@ if odpowiedz.status_code == 200:
         linki = div_strony.find_all("a")
 
         for link in linki:
-
-            if "title" in link.attrs:
-
+            if "title" in link.attrs and not link["href"].startswith("/wiki/Kategoria:"):
                 url_artykulu = link["href"]
                 nazwa_artykulu = link["title"]
                 artykuly.append({"url": url_artykulu, "nazwa": nazwa_artykulu})
@@ -38,49 +36,51 @@ if odpowiedz.status_code == 200:
                     for link in linki:
                         tytul = link.get('title')
                         tekst = link.get_text(strip=True)
-                        if tytul and tekst:
+                        if tytul and tekst and not tytul.startswith("Kategoria:"):
                             tytuly.append(tytul)
                             if len(tytuly) == 5:
                                 break
 
                 div_tresc = zupa_artykul.find("div", {"class": "mw-content-ltr mw-parser-output"})
-                obrazy = div_tresc.find_all("img", src=True)
-                url_obrazow = [img["src"] for img in obrazy[:3]]
-                przypisy_h2 = zupa_artykul.find('span', {"id": "Przypisy"})
-
-                if przypisy_h2:
-                    przypisy = przypisy_h2.find_next("ol", {"class": "references"})
-                    zewnetrzne_linki = przypisy.find_all('a', {"class": 'external text'})
-                    url_przypisow = [link['href'] for link in zewnetrzne_linki[:3]]
-                    url_przypisow = [url.replace("&", "&amp;") for url in url_przypisow]
-
+                if div_tresc:
+                    obrazy = div_tresc.find_all("img", src=True)
+                    url_obrazow = [img["src"] for img in obrazy[:3]]
                 else:
-                    url_przypisow = []
+                    url_obrazow = []
 
-                kategorie = zupa_artykul.find("div", {"id": "mw-normal-catlinks"}).find_all("a")
-                nazwy_kategorii = [kat.get_text() for kat in kategorie[1:4]]
+                # Zbieranie linków zewnętrznych z całej treści, z wykluczeniem niechcianych domen
+                zewnetrzne_linki = zupa_artykul.find_all('a', href=True)
+                url_przypisow = []
+                for link in zewnetrzne_linki:
+                    if (link['href'].startswith('http') and
+                        'wikipedia.org' not in link['href'] and
+                        'wikidata.org' not in link['href'] and
+                        'mediawiki.org' not in link['href'] and
+                        'wikibooks.org' not in link['href'] and
+                        'commons.wikimedia.org' not in link['href'] and
+                        'creativecommons.org' not in link['href'] and
+                        'foundation.wikimedia.org' not in link['href']):
+                        url_przypisow.append(link['href'])
+                    if len(url_przypisow) >= 3:
+                        break
 
-                sformatowany_tytul = ""
-                sformatowany_url_obrazu = ""
-                sformatowany_url_przypisu = ""
-                sformatowana_nazwa_kategorii = ""
-                for tytul in tytuly:
-                    sformatowany_tytul += tytul + " | "
-                sformatowany_tytul = sformatowany_tytul[:-3]
+                url_przypisow = [url.replace("&", "&amp;") for url in url_przypisow]
+
+                kategorie = zupa_artykul.find("div", {"id": "mw-normal-catlinks"})
+                if kategorie:
+                    nazwy_kategorii = [kat.get_text() for kat in kategorie.find_all("a")[1:4]]
+                else:
+                    nazwy_kategorii = []
+
+                # Formatowanie i wyświetlanie wyników
+                sformatowany_tytul = " | ".join(tytuly) if tytuly else ""
+                sformatowany_url_obrazu = " | ".join(url_obrazow) if url_obrazow else ""
+                sformatowany_url_przypisu = " | ".join(url_przypisow) if url_przypisow else ""
+                sformatowana_nazwa_kategorii = " | ".join(nazwy_kategorii) if nazwy_kategorii else ""
+
                 print(sformatowany_tytul)
-                for url_obraz in url_obrazow:
-                    sformatowany_url_obrazu += url_obraz + " | "
-                sformatowany_url_obrazu = sformatowany_url_obrazu[:-3]
                 print(sformatowany_url_obrazu)
-
-                for url_przypis in url_przypisow:
-                    sformatowany_url_przypisu += url_przypis + " | "
-                sformatowany_url_przypisu = sformatowany_url_przypisu[:-3]
                 print(sformatowany_url_przypisu)
-
-                for nazwa_kategorii in nazwy_kategorii:
-                    sformatowana_nazwa_kategorii += nazwa_kategorii + " | "
-                sformatowana_nazwa_kategorii = sformatowana_nazwa_kategorii[:-3]
                 print(sformatowana_nazwa_kategorii)
 
             else:
