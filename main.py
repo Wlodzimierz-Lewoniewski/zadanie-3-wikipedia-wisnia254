2c8d44e21ab77429d480c1b28eb89cf276976261
Mateusz Wisniewski <83494@student.ue.poznan.pl>
2024-11-03 15:20:16 +0100

    3

--- main.py
+++ main.py
@@ -1,91 +1,73 @@
 import requests
 from bs4 import BeautifulSoup
 
-nazwa_kategorii = input("Podaj nazwę kategorii: ")
-nazwa_kategorii = nazwa_kategorii.replace(" ", "_")
-url = "https://pl.wikipedia.org/wiki/Kategoria:" + nazwa_kategorii
-odpowiedz = requests.get(url)
+def szukaj():
+    nazwa_kategorii = input("Podaj nazwę kategorii Wikipedii: ")
+    url = "https://pl.wikipedia.org/wiki/Kategoria:" + nazwa_kategorii.replace(' ', '_')
+    odpowiedz = requests.get(url)
 
-if odpowiedz.status_code == 200:
+    if odpowiedz.status_code == 200:
+        zupa = BeautifulSoup(odpowiedz.text, "html.parser")
+        strony_div = zupa.find("div", id="mw-pages")
 
-    zupa = BeautifulSoup(odpowiedz.text, "html.parser")
-    div_strony = zupa.find("div", id="mw-pages")
+        if strony_div:
+            artykuly = [
+                {"url": link["href"], "nazwa": link["title"]}
+                for link in strony_div.find_all("a") if "title" in link.attrs
+            ]
 
-    if div_strony:
-        artykuly = []
-        linki = div_strony.find_all("a")
+            for idx in range(2):
+                if idx < len(artykuly):
+                    artykul = artykuly[idx]
+                    pelny_url = "https://pl.wikipedia.org" + artykul["url"]
+                    odpowiedz_artykul = requests.get(pelny_url)
+                    zupa_artykul = BeautifulSoup(odpowiedz_artykul.text, "html.parser")
 
-        for link in linki:
-            if "title" in link.attrs and not link["href"].startswith("/wiki/Kategoria:"):
-                url_artykulu = link["href"]
-                nazwa_artykulu = link["title"]
-                artykuly.append({"url": url_artykulu, "nazwa": nazwa_artykulu})
+                    zawartosc = zupa_artykul.find('div', {'id': 'mw-content-text'})
+                    tytuly = []
+                    if zawartosc:
+                        znaczniki_a = zawartosc.select('a:not(.extiw)')
+                        tytuly = [znacznik.get('title') for znacznik in znaczniki_a if znacznik.get('title') and znacznik.get_text(strip=True)]
+                        tytuly = tytuly[:5]
 
-        for i in range(2):
+                    div_tresc = zupa_artykul.find("div", {"class": "mw-parser-output"})
+                    url_obrazow = []
+                    if div_tresc:
+                        znaczniki_obrazow = div_tresc.find_all("img", src=True)
+                        url_obrazow = [obraz["src"] for obraz in znaczniki_obrazow[:3]]
 
-            if i < len(artykuly):
-                artykul = artykuly[i]
-                url_artykulu = "https://pl.wikipedia.org" + artykul["url"]
-                odpowiedz_artykul = requests.get(url_artykulu)
-                zupa_artykul = BeautifulSoup(odpowiedz_artykul.text, "html.parser")
-                tytuly = []
-                kontener_div = zupa_artykul.find('div', {'id': 'mw-content-text', 'class': 'mw-body-content'})
-
-                if kontener_div:
-                    linki = kontener_div.select('a:not(.extiw)')
-                    for link in linki:
-                        tytul = link.get('title')
-                        tekst = link.get_text(strip=True)
-                        if tytul and tekst and not tytul.startswith("Kategoria:"):
-                            tytuly.append(tytul)
-                            if len(tytuly) == 5:
+                    przypisy_div = zupa_artykul.find("div", {"class": "mw-references-wrap"})
+                    url_przypisow = []
+                    if przypisy_div:
+                        linki_przypisow = przypisy_div.find_all('a', class_='external text')
+                        for link in linki_przypisow:
+                            url_przypisow.append(link.get('href'))
+                            if len(url_przypisow) == 3:
                                 break
 
-                div_tresc = zupa_artykul.find("div", {"class": "mw-content-ltr mw-parser-output"})
-                if div_tresc:
-                    obrazy = div_tresc.find_all("img", src=True)
-                    url_obrazow = [img["src"] for img in obrazy[:3]]
-                else:
-                    url_obrazow = []
+                    url_przypisow = [url.replace("&", "&amp;") for url in url_przypisow]
 
-                # Zbieranie linków zewnętrznych z całej treści, z wykluczeniem niechcianych domen
-                zewnetrzne_linki = zupa_artykul.find_all('a', href=True)
-                url_przypisow = []
-                for link in zewnetrzne_linki:
-                    if (link['href'].startswith('http') and
-                        'wikipedia.org' not in link['href'] and
-                        'wikidata.org' not in link['href'] and
-                        'mediawiki.org' not in link['href'] and
-                        'wikibooks.org' not in link['href'] and
-                        'commons.wikimedia.org' not in link['href'] and
-                        'creativecommons.org' not in link['href'] and
-                        'foundation.wikimedia.org' not in link['href']):
-                        url_przypisow.append(link['href'])
-                    if len(url_przypisow) >= 3:
-                        break
-
-                url_przypisow = [url.replace("&", "&amp;") for url in url_przypisow]
-
-                kategorie = zupa_artykul.find("div", {"id": "mw-normal-catlinks"})
-                if kategorie:
-                    nazwy_kategorii = [kat.get_text() for kat in kategorie.find_all("a")[1:4]]
-                else:
+                    div_kategorie = zupa_artykul.find("div", {"id": "mw-normal-catlinks"})
                     nazwy_kategorii = []
+                    if div_kategorie:
+                        linki_kategorii = div_kategorie.find_all("a")
+                        nazwy_kategorii = [kategoria.get_text() for kategoria in linki_kategorii[1:4]]
 
-                # Formatowanie i wyświetlanie wyników
-                sformatowany_tytul = " | ".join(tytuly) if tytuly else ""
-                sformatowany_url_obrazu = " | ".join(url_obrazow) if url_obrazow else ""
-                sformatowany_url_przypisu = " | ".join(url_przypisow) if url_przypisow else ""
-                sformatowana_nazwa_kategorii = " | ".join(nazwy_kategorii) if nazwy_kategorii else ""
+                    wynik_tytuly = " | ".join(tytuly)
+                    wynik_obrazy = " | ".join(url_obrazow)
+                    wynik_przypisy = " | ".join(url_przypisow)
+                    wynik_kategorie = " | ".join(nazwy_kategorii)
 
-                print(sformatowany_tytul)
-                print(sformatowany_url_obrazu)
-                print(sformatowany_url_przypisu)
-                print(sformatowana_nazwa_kategorii)
-
-            else:
-                print(f"Artykuł {i + 1}: Brak informacji")
+                    print(wynik_tytuly)
+                    print(wynik_obrazy)
+                    print(wynik_przypisy)
+                    print(wynik_kategorie)
+                else:
+                    print(f"Artykuł {idx + 1}: Informacje nie znalezione")
+        else:
+            print("Nie znaleziono stron")
     else:
-        print("Brak sekcji mw-pages")
-else:
-    print(f"Kod statusu: {odpowiedz.status_code}")
+        print(f"Kod statusu: {odpowiedz.status_code}")
+
+if __name__ == "__main__":
+    szukaj()
